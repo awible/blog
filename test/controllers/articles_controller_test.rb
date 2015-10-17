2415ae1e0fad999fe2c0ea94bddef9553ed7e44b
Adrian Wible <apwible@gmail.com>
2015-10-17 17:57:45 -0400

    test data + initial controller test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:articles)
+  end
+
+#  test "should show article" do
+#    get :article
+#    assert_response :success
+#    assert_redirected_to(controller: "ArticlesController", action: "show)
+#  end
+
+  test "should_create_article" do
+    assert (post :create, article: {title: "Long Enough", text: "Wisdom"})
+    # assert_redirect_to article_path(assigns(:article))    
+  end
 end
