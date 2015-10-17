e010f79e196e8eed37c359f3e3ff460081275941
Adrian Wible <apwible@gmail.com>
2015-10-17 14:49:36 -0400

    Add Article tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -6,13 +6,33 @@ class ArticleTest < ActiveSupport::TestCase
   # end
 
   test "article_name_fails_with_4_characters" do
-    a = Article.new(:title => "test")
+    a = Article.new
+    assert (!a.valid?)
+    a.title = "test"
     assert (!a.valid?)
   end
 
   test "article_name_succeeds_with_5_characters" do
     a = Article.new(:title => "test1")
     assert (a.valid?)
+  end
+
+  test "article_gets_destroyed" do
+    a = Article.new(:title => "test1")
+    a.comments << Comment.new(:commenter => "Adrian", :body => "Adrian, you are brilliant!")
+    a.save
+    a.destroy
+    assert (a.destroyed?)
+  end
+
+  test "comments_get_destroyed_when_parent_destroyed" do
+    a = Article.new(:title => "test1")
+    a.comments << Comment.new(:commenter => "Adrian", :body => "Adrian, you are brilliant!")
+    a.comments << Comment.new(:commenter => "Adrian", :body => "Adrian, you are really brilliant!")
+    a.save
+    assert (a.comments.size == 2)
+    a.destroy
+    assert (a.comments.size == 0)
   end
 
 end
